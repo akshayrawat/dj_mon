35ebc77ac3c15b2de5799200de9011d7d745d543
Ellis Berner <eberner@gmail.com>
2012-09-10 12:37:48 -0700

    Update lib/dj_mon/backend/activerecord.rb

--- lib/dj_mon/backend/activerecord.rb
+++ lib/dj_mon/backend/activerecord.rb
@@ -3,29 +3,29 @@ module DjMon
     module ActiveRecord
       class << self
         def all
-          Delayed::Job.all
+          Delayed::Job.scoped
         end
 
         def failed
-          Delayed::Job.where('delayed_jobs.failed_at IS NOT NULL')
+          Delayed::Job.where('delayed_jobs.failed_at IS NOT NULL').last(500)
         end
 
         def active
-          Delayed::Job.where('delayed_jobs.failed_at IS NULL AND delayed_jobs.locked_by IS NOT NULL')
+          Delayed::Job.where('delayed_jobs.failed_at IS NULL AND delayed_jobs.locked_by IS NOT NULL').last(500)
         end
 
         def queued
-          Delayed::Job.where('delayed_jobs.failed_at IS NULL AND delayed_jobs.locked_by IS NULL')
+          Delayed::Job.where('delayed_jobs.failed_at IS NULL AND delayed_jobs.locked_by IS NULL').last(500)
         end
 
         def destroy id
           dj = Delayed::Job.find(id)
-          dj.destroy if dj
+          dj.destroy
         end
 
         def retry id
           dj = Delayed::Job.find(id)
-          dj.update_attributes({ failed_at: nil }) if dj
+          dj.update_attributes({ failed_at: nil })
         end
       end
     end
