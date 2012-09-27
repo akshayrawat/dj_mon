5827696dbf4350d6a29dad0e0475eae24a2e7077
Timo Schilling <timo@schilling.io>
2012-09-27 02:52:09 +0200

    add open_in_ios_app_url helper and use it for Apple App Banner

--- app/helpers/dj_mon/ios_app_helper.rb
+++ app/helpers/dj_mon/ios_app_helper.rb
@@ -1,4 +1,12 @@
 module DjMon
   module IosAppHelper
+    def open_in_ios_app_url
+      params = {}
+      params[:user] = Rails.configuration.dj_mon.username
+      params[:password] = Rails.configuration.dj_mon.password
+      params[:name] = request.domain.split(".").first.humanize
+      params[:host] = dj_mon.root_url
+      "dj-mon://open?#{params.map{|param, value| "#{param}=#{value}" }.join("&")}"
+    end
   end
 end
